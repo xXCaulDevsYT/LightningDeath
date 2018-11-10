ac51f0dd21a19e972ca4ce36bc3376324e90a46d
xXCaulDevsYT <34898438+xXCaulDevsYT@users.noreply.github.com>
2018-11-10 16:40:19 -0600

    Create Main.php

---
+++ EmeraldMC/LightningDeath/Main.php
@@ -0,0 +1,35 @@
+<?php
+namespace EmeraldMC\LightningDeath;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\Listener;
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\entity\Entity;
+use pocketmine\network\protocol\AddEntityPacket;
+
+class Main extends PluginBase implements Listener
+{
+
+  public function onEnable(){
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  $this->getLogger()->info("TextFormat::AQUA . LightningDeath Has Successfully Loaded In!");
+}
+
+
+  public function onDeath(PlayerDeathEvent $ent)
+  {
+    $p = $e->getPlayer();
+    $light = new AddEntityPacket();
+    $light->type = 93;
+    $light->eid = Entity::$entityCount++;
+    $light->metadata = array();
+    $light->speedX = 0;
+    $light->speedY = 0;
+    $light->speedZ = 0;
+    $light->x = $p->x;
+    $light->y = $p->y;
+    $light->z = $p->z;
+    $p->dataPacket($light);
+  }
+}  
